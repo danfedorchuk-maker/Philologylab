0910d39df7e42c0026bdee0798059100f8ca2200
danfedorchuk-maker <dan.fedorchuk@gmail.com>
2026-04-26 00:02:31 -0700

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-res.setHeader('Access-Control-Allow-Origin', 'https://philologylab.vercel.app');
+res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Max-Age', '86400');
+  res.setHeader('Access-Control-Allow-Origin', 'https://philologylab.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
